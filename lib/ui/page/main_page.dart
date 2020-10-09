3d89dc5aaceb830773768052f85af87649a21f88
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 13:25:39 +0700

    Update main_page.dart

--- lib/ui/page/main_page.dart
+++ lib/ui/page/main_page.dart
@@ -1,1 +1,25 @@
 part of 'pages.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            AuthServices.signOut();
+          },
+          child: Text("Sign out"),
+        ),
+      ),
+    );
+  }
+}

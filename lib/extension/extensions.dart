5d104c813bbd6694ecaf079f26f878d2abe0c819
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 08:52:53 +0700

    Update extensions.dart

---
+++ lib/extension/extensions.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tix_apps/model/models.dart';
+import 'package:tix_apps/service/services.dart';
+
+part 'firebase_user_extension.dart';

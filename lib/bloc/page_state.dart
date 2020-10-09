da5eb27df3f22acaf473ba4036de199735765ac0
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 13:17:33 +0700

    Create page_state.dart

---
+++ lib/bloc/page_state.dart
@@ -0,0 +1,16 @@
+part of 'page_bloc.dart';
+
+abstract class PageState extends Equatable {
+  const PageState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class OnInitialPage extends PageState {}
+
+class OnSplashPage extends PageState {}
+
+class OnLoginPage extends PageState {}
+
+class OnMainPage extends PageState {}

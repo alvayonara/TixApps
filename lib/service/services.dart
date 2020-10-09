b000e3e3cddde241695f31b22900f934a6018b42
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 08:49:57 +0700

    Configure firebase in Android gradles

---
+++ lib/service/services.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tix_apps/model/models.dart';
+import 'package:tix_apps/extension/extensions.dart';
+
+part 'auth_services.dart';
+part 'user_services.dart';

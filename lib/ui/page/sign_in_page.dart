bd0fc1fe2c6dab20e9ea54c94cf661b61329b3b5
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 13:25:25 +0700

    Update sign_in_page.dart

--- lib/ui/page/sign_in_page.dart
+++ lib/ui/page/sign_in_page.dart
@@ -1,1 +1,25 @@
 part of 'pages.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In Page"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            AuthServices.signIn("alfred@gmail.com", "123456");
+          },
+          child: Text("Sign In"),
+        ),
+      ),
+    );
+  }
+}

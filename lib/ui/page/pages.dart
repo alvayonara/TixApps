733d59c5a70424a5bddc32c53af845d5ad5c6a91
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 13:26:12 +0700

    Update pages.dart

--- lib/ui/page/pages.dart
+++ lib/ui/page/pages.dart
@@ -1,3 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tix_apps/bloc/blocs.dart';
+import 'package:tix_apps/service/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tix_apps/shared/shared.dart';
+
 part 'sign_in_page.dart';
 part 'wrapper.dart';
 part 'main_page.dart';
+part 'splash_page.dart';

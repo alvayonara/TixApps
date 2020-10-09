1c20fd16801692a39d23ae41b09642216534f7c4
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-09 08:52:41 +0700

    Create extension for FirebaseUser

---
+++ lib/extension/firebase_user_extension.dart
@@ -0,0 +1,17 @@
+part of 'extensions.dart';
+
+extension FirebaseUserExtension on FirebaseUser {
+  // Set default value for name, selectedGenres, selectedLanguages and balance
+  User convertToUser(
+          {String name = "No Name",
+          List<String> selectedGenres = const [],
+          String selectedLanguage = "English",
+          int balance = 50000}) =>
+      User(this.uid, this.email,
+          name: name,
+          balance: balance,
+          selectedGenres: selectedGenres,
+          selectedLanguage: selectedLanguage);
+
+  Future<User> fromFirestore() async => await UserServices.getUser(this.uid);
+}

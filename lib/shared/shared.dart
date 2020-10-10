214bd3901becd7bef22c965b085009bff696f3d6
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-10 07:11:20 +0700

    Update shared.dart
    Import theme.dart

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tix_apps/bloc/page_bloc.dart';
 
 part 'shared_value.dart';
+part 'theme.dart';

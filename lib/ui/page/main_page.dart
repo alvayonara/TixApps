f1593aed2b5aaa56c7d68cfc94328eb8bd5d6a4b
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-11 09:26:44 +0700

    Implement Bottom Navigation Bar in main_page.dart

--- lib/ui/page/main_page.dart
+++ lib/ui/page/main_page.dart
@@ -6,30 +6,179 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // Set bottom navigation bar
+  int bottomNavBarIndex;
+
+  // Set page controller based on index of bottom nav bar
+  PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initial index = 0 in New Movies page
+    bottomNavBarIndex = 0;
+
+    // Initial page controller in New Movies page
+    pageController = PageController(initialPage: bottomNavBarIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main page"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<UserBloc, UserState>(
-              builder: (_, userState) => (userState is UserLoaded)
-                  ? Text(userState.user.name)
-                  : SizedBox(),
+      body: Stack(
+        children: [
+          // Stack 1 -> For status bar color
+          Container(
+            color: accentColor1,
+          ),
+          // Stack 2 -> For main page exclude status bar
+          SafeArea(
+            child: Container(
+              color: Color(0xFFF6F7F9),
             ),
-            RaisedButton(
-              onPressed: () {
-                AuthServices.signOut();
-              },
-              child: Text("Sign out"),
+          ),
+          // Stack 3 -> Change Page based on bottom navigation clicked
+          PageView(
+            controller: pageController,
+            children: [
+              // Index 0 -> New Movies
+              Center(
+                child: Text("Page New Movies"),
+              ),
+              // Index 1 -> My Ticket
+              Center(
+                child: Text("Page My Ticket"),
+              ),
+            ],
+            // Using setState to change Page based on index value (bottom nav bar index)
+            onPageChanged: (index) {
+              setState(() {
+                bottomNavBarIndex = index;
+              });
+            },
+          ),
+          // Stack 4 -> For bottom navigation layout
+          createCustomBottomNavBar(),
+          // Stack 5 -> For add wallet button above bottom navigaton
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 46,
+              width: 46,
+              margin: EdgeInsets.only(bottom: 42),
+              child: FloatingActionButton(
+                elevation: 0,
+                backgroundColor: accentColor2,
+                child: SizedBox(
+                  height: 26,
+                  width: 26,
+                  child: Icon(
+                    MdiIcons.walletPlus,
+                    color: Colors.black.withOpacity(0.54),
+                  ),
+                ),
+                onPressed: () {},
+              ),
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
+
+  // Widget for bottom navigation
+  Widget createCustomBottomNavBar() => Align(
+        alignment: Alignment.bottomCenter,
+        child: ClipPath(
+          clipper: BottomNavBarClipper(),
+          child: Container(
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: BottomNavigationBar(
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              selectedItemColor: mainColor,
+              unselectedItemColor: Color(0xFFE5E5E5),
+              currentIndex: bottomNavBarIndex,
+              items: [
+                BottomNavigationBarItem(
+                  title: Text(
+                    "New Movies",
+                    style: GoogleFonts.raleway(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  icon: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 6,
+                    ),
+                    height: 20,
+                    child: Image.asset(
+                      (bottomNavBarIndex == 0)
+                          ? "assets/ic_movie.png"
+                          : "assets/ic_movie_grey.png",
+                    ),
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  title: Text(
+                    "My Tickets",
+                    style: GoogleFonts.raleway(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  icon: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 6,
+                    ),
+                    height: 20,
+                    child: Image.asset(
+                      (bottomNavBarIndex == 1)
+                          ? "assets/ic_tickets.png"
+                          : "assets/ic_tickets_grey.png",
+                    ),
+                  ),
+                ),
+              ],
+              onTap: (index) {
+                setState(() {
+                  bottomNavBarIndex = index;
+
+                  pageController.jumpToPage(index);
+                });
+              },
+            ),
+          ),
+        ),
+      );
+}
+
+// Clipper for bottom navigation
+class BottomNavBarClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+
+    path.lineTo(size.width / 2 - 28, 0);
+    path.quadraticBezierTo(size.width / 2 - 28, 33, size.width / 2, 33);
+    path.quadraticBezierTo(size.width / 2 + 28, 33, size.width / 2 + 28, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) => false;
 }
--- lib/ui/page/pages.dart
+++ lib/ui/page/pages.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:tix_apps/bloc/blocs.dart';
 import 'package:tix_apps/main.dart';

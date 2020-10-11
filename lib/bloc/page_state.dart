bbf0e24eab0e22560fdc023577983df609e4ad80
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-11 17:24:17 +0700

    Create new state in PageState (OnRegistrationPage, OnPreferencesPage, OnAccountConfirmationPage)

--- lib/bloc/page_state.dart
+++ lib/bloc/page_state.dart
@@ -11,6 +11,24 @@ class OnInitialPage extends PageState {}
 
 class OnSplashPage extends PageState {}
 
+class OnRegistrationPage extends PageState {
+  final RegistrationData registrationData;
+
+  OnRegistrationPage(this.registrationData);
+}
+
+class OnPreferencesPage extends PageState {
+  final RegistrationData registrationData;
+
+  OnPreferencesPage(this.registrationData);
+}
+
+class OnAccountConfirmationPage extends PageState {
+  final RegistrationData registrationData;
+
+  OnAccountConfirmationPage(this.registrationData);
+}
+
 class OnLoginPage extends PageState {}
 
 class OnMainPage extends PageState {}

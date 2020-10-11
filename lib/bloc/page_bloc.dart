cfaff1f6b4e1e81be60b7fc64a7046e13a94a0f3
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-11 17:26:03 +0700

    Update PageBloc (Registration, Preferences, AccountConfirmation)

--- lib/bloc/page_bloc.dart
+++ lib/bloc/page_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:tix_apps/model/models.dart';
 
 part 'page_event.dart';
 part 'page_state.dart';
@@ -18,12 +19,21 @@ class PageBloc extends Bloc<PageEvent, PageState> {
     // Event -> GoToSplashPage then load OnSplashPage() state
     // Event -> GoToLoginPage then load OnLoginPage() state
     // Event -> GoToMainPage then load OnMainPage() state
+    // Event -> GoToRegistrationPage then load OnRegistrationPage() state
+    // Event -> GoToPreferencesPage then load OnPreferencesPage() state
+    // Event -> GoToAccountConfirmationPage then load OnAccountConfirmationPage() state
     if (event is GoToSplashPage) {
       yield OnSplashPage();
     } else if (event is GoToLoginPage) {
       yield OnLoginPage();
     } else if (event is GoToMainPage) {
       yield OnMainPage();
+    } else if (event is GoToRegistrationPage) {
+      yield OnRegistrationPage(event.registrationData);
+    } else if (event is GoToPreferencesPage) {
+      yield OnPreferencesPage(event.registrationData);
+    } else if (event is GoToAccountConfirmationPage) {
+      yield OnAccountConfirmationPage(event.registrationData);
     }
   }
 }

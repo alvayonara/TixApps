1feef56b7dfe4fce8b70e6bb76529a9359e513da
Alva Yonara Puramandya <alvayonara@outlook.com>
2020-10-11 17:24:51 +0700

    Create new event in PageEvent (GoToRegistrationPage, GoToPreferencesPage, GoToAccountConfirmationPage)

--- lib/bloc/page_event.dart
+++ lib/bloc/page_event.dart
@@ -18,3 +18,30 @@ class GoToMainPage extends PageEvent {
   @override
   List<Object> get props => [];
 }
+
+class GoToRegistrationPage extends PageEvent {
+  final RegistrationData registrationData;
+
+  GoToRegistrationPage(this.registrationData);
+
+  @override
+  List<Object> get props => [];
+}
+
+class GoToPreferencesPage extends PageEvent {
+  final RegistrationData registrationData;
+
+  GoToPreferencesPage(this.registrationData);
+
+  @override
+  List<Object> get props => [];
+}
+
+class GoToAccountConfirmationPage extends PageEvent {
+  final RegistrationData registrationData;
+
+  GoToAccountConfirmationPage(this.registrationData);
+
+  @override
+  List<Object> get props => [];
+}
